9103c1e32b369d776009b28108eaaa5150ad6aa1
Jorge Tinoco <iorifonseca777@gmail.com>
2020-12-01 10:54:48 -0500

    solucionado ejercicio 8, falta estudiar ejercicio 9 conceptos no muy claros

---
+++ exercice/message.py
@@ -0,0 +1,19 @@
+# print("como te llamas: ", end="")
+# name = input()
+# print(f"como estas {name}")
+
+## forma acortada
+# name2 = input("como te llamas: ")
+# print(f"como estas {name2}")
+
+## si quieres usar int en input, lo mismo para float
+# name3 = int(input("ingrese su sueldo: "))
+# print(f"su sueldo es de: {name3} " + f" menos el descuento de iva que es: {(name3 / 0.83) - name3}")
+# print(name3)
+
+
+## Mostrar 2 variables o mas en un print
+
+nombre = input("Escriba su Nombre  por favor: ")
+apellido = input (f"Escriba su apellido señor {nombre}: ")
+print(f"Bienvenido señor {nombre} {apellido}")
